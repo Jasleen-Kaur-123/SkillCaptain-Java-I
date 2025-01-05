1fcc0257e7162e5f978d7f3deac63641d5229286
Jasleen-Kaur-123 <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2025-01-05 12:19:39 +0000

    first commit

---
+++ PrimitiveDataType.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       int age = 25;
+       String name = "Skill Captain";
+       System.out.println("Hello," +name+"! You are "+age+" years old.");
+    }
+}
